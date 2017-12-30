fb5ef774fb6594fd7f4eff7ff5035e47835383ee
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-12-29 20:50:45 -0500

    creating models

---
+++ db/migrate/20171230014351_create_notes.rb
@@ -0,0 +1,10 @@
+class CreateNotes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :notes do |t|
+      t.string :content
+      t.string :time_of_day
+      t.belongs_to :coffee
+      t.timestamps
+    end
+  end
+end
