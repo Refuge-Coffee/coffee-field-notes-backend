fe9d81dc403485b800411b836a961b0d4ec58039
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-12-29 23:20:44 -0500

    content type time

--- app/controllers/api/v1/coffee_controller.rb
+++ app/controllers/api/v1/coffee_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::CoffeeController < JSONAPI::ResourceController
   def index
     @coffee = Coffee.all
-    JSONAPI::ResourceSerializer.new(Api::V1::CoffeeResource).serialize_to_hash(Api::V1::CoffeeResource.new(coffee, nil))
+    @coffee = @coffee.map { |coffee| Api::V1::CoffeeResource.new(coffee, nil) }
+    render json: JSONAPI::ResourceSerializer.new( Api::V1::CoffeeResource).serialize_to_hash(@coffee),
+      content_type: "text/json"
   end
 
   def show
