d7783c40abffb68d1489bd6bcb787f0b83fbd083
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-01-01 21:58:57 -0500

    working on the api side of creation

--- app/controllers/api/v1/coffee_controller.rb
+++ app/controllers/api/v1/coffee_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CoffeeController < JSONAPI::ResourceController
   def index
-    @coffee = Coffee.all.limit(9)
+    @coffee = Coffee.last(9)
     @coffee = @coffee.map { |coffee| Api::V1::CoffeeResource.new(coffee, nil) }
     render json: JSONAPI::ResourceSerializer.new( Api::V1::CoffeeResource).serialize_to_hash(@coffee),
       content_type: "text/json"
@@ -11,7 +11,31 @@ class Api::V1::CoffeeController < JSONAPI::ResourceController
     render json: serialize_coffee(@coffee)
   end
 
+  def create
+    @coffee = Coffee.new(build_coffee_params(request_json["data"]))
+    if @coffee.save
+      render json: serialize_coffee(@coffee), content_type: "text/json", status: 201
+    else
+      render json: serialize_coffee(@coffee), content_type: "text/json", status: 500
+    end
+  end
+
 private
+
+  def build_coffee_params(data)
+    coffee_params = ActionController::Parameters.new(data)
+    attributes = [
+      :name, :origin, :elevation
+    ]
+
+    coffee_params.permit(
+      attributes: attributes
+    )[:attributes]
+  end
+
+  def request_json
+    @request_json ||= JSON.parse(request.body.read)
+  end
 
   def serialize_coffee(coffee)
     JSONAPI::ResourceSerializer.new(Api::V1::CoffeeResource).serialize_to_hash(Api::V1::CoffeeResource.new(coffee, nil))
--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,2 +1,5 @@
 class Coffee < ApplicationRecord
+  validates :name, presence: true
+  validates :origin, presence: true
+  validates :elevation, presence: true
 end
