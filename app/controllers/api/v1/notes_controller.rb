e86e1dfe97c3b2b8ddc681c18decc2b1c2bc8ef2
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-03-30 23:18:14 -0400

    adding in notes controller and resources

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,2 +1,43 @@
 class Api::V1::NotesController < JSONAPI::ResourceController
+  def index
+    @notes = Note.all.limit(25)
+    @notes = @notes.map { |note| Api::V1::BeanResource.new(note, nil) }
+    render json: JSONAPI::ResourceSerializer.new(Api::V1::NoteResource).serialize_to_hash(@notes),
+      content_type: "text/json"
+  end
+
+  def show
+    @note = Note.find(params[:id])
+    render json: serialize_note(@note)
+  end
+
+  def create
+    @note = Note.new(build_note_params(request_json["data"]))
+    if @note.save
+      render json: serialize_note(@note), content_type: "text/json", status: 201
+    else
+      render json: serialize_note(@note), content_type: "text/json", status: 500
+    end
+  end
+
+private
+
+  def build_note_params(data)
+    bean_params = ActionController::Parameters.new(data)
+    attributes = [
+      :content, :time_of_day, :association
+    ]
+
+    note_params.permit(
+      attributes: attributes
+    )[:attributes]
+  end
+
+  def request_json
+    @request_json ||= JSON.parse(request.body.read)
+  end
+
+  def serialize_note(note)
+    JSONAPI::ResourceSerializer.new(Api::V1::NoteResource).serialize_to_hash(Api::V1::NoteResource.new(note, nil))
+  end
 end
