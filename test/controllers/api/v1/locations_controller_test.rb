6511eadfbeda239a4caa0bcb2845853e0e80e916
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-04-28 22:05:57 -0400

    updating controller tests

--- test/controllers/api/v1/locations_controller_test.rb
+++ test/controllers/api/v1/locations_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class Api::V1::LocationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @roasters = FactoryBot.create_list(:location, 15)
+    @roaster = @locations.first
+  end
+
+  test "GET Index" do
+    get api_v1_locations_url
+    assert_response :success
+  end
+
+  test "GET Show" do
+    get api_v1_location_url(@location.id)
+    assert_response :success
+  end
 end
---
+++ test/controllers/api/v1/roasters_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class Api::V1::RoastersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @roasters = FactoryBot.create_list(:roaster, 15)
+    @roaster = @roasters.first
+  end
+
+  test "GET Index" do
+    get api_v1_roasters_url
+    assert_response :success
+  end
+
+  test "GET Show" do
+    get api_v1_roaster_url(@roaster.id)
+    assert_response :success
+  end
+end
