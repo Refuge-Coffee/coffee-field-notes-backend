71f2c924c4b654ba6e163ba25a5b8d004e7dee8d
Taylor Jones <taylorjonessoftware@gmail.com>
2018-05-06 20:04:47 -0500

    roaster things

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  resources :roasters
-  namespace :api do
-    namespace :v1 do
-      resources :locations
-    end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       jsonapi_resources :beans
       jsonapi_resources :notes
+      jsonapi_resources :locations
+      jsonapi_resources :roasters
     end
   end
 end
